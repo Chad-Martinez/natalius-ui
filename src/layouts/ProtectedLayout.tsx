0fc0782bd20df9d4a4f39858db005f4d8202c942
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-23 17:56:23 -0600

    created top navbar and injected title

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,19 +1,30 @@
-import { FC, useContext, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { FC, useContext, useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../store/AuthContext';
 import TopNav from '../components/dashboard/TopNav';
 
 const ProtectedLayout: FC = (): JSX.Element => {
+  const [title, setTitle] = useState<string>('');
   const { isAuth } = useContext(AuthContext);
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!isAuth) return navigate('/login');
   }, [navigate, isAuth]);
 
+  useEffect(() => {
+    const paths = pathname.split('/');
+    if (paths.length === 2) {
+      setTitle(paths[1]);
+    } else {
+      setTitle(paths[2]);
+    }
+  }, [pathname]);
+
   return (
     <div>
-      <TopNav />
+      <TopNav title={title} />
       <Outlet />
     </div>
   );
