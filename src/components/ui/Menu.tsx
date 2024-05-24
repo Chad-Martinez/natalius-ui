4b8d769a67f6e073f0f744f13097401bcd00c4a7
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-24 00:53:00 -0600

    implemented useContext to toggle menu dropdown

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,13 +1,24 @@
-import { FC } from 'react';
+import { FC, SyntheticEvent, useContext } from 'react';
 import styles from './Menu.module.css';
+import MenuContainer from './MenuContainer';
+import { MenuContext } from '../../layouts/ProtectedLayout';
 
 const Menu: FC = (): JSX.Element => {
+  const { isOpen, setIsOpen } = useContext(MenuContext);
+
+  const toggleMenu = (event: SyntheticEvent) => {
+    event.stopPropagation();
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className={styles.menu}>
-      <div className={styles.menuLine}></div>
-      <div className={styles.menuLine}></div>
-      <div className={styles.menuLine}></div>
-    </div>
+    <>
+      <div className={styles.menu} onClick={toggleMenu}>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+        <MenuContainer open={isOpen} />
+      </div>
+    </>
   );
 };
 
