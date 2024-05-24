526d68e48ea8f653fd746ffc3db3ae02efe548b3
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-23 21:52:26 -0600

    refactored context to use setState function - removed unnecessary useMemo hook

--- src/store/AuthContext.tsx
+++ src/store/AuthContext.tsx
@@ -1,33 +1,24 @@
 import {
+  Dispatch,
   PropsWithChildren,
+  SetStateAction,
   createContext,
-  useCallback,
-  useMemo,
   useState,
 } from 'react';
 
-export const AuthContext = createContext({
+export const AuthContext = createContext<{
+  isAuth: boolean;
+  setIsAuth: Dispatch<SetStateAction<boolean>> | ((value: boolean) => void);
+}>({
   isAuth: false,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  updateAuth: (value: boolean) => {},
+  setIsAuth: () => {},
 });
 
 export const AuthProvider = (props: PropsWithChildren) => {
   const [isAuth, setIsAuth] = useState(false);
 
-  const updateAuth = useCallback((value: boolean): void => {
-    setIsAuth(value);
-  }, []);
-
-  const authStore = useMemo(
-    () => ({
-      isAuth,
-      updateAuth,
-    }),
-    [isAuth, updateAuth]
-  );
   return (
-    <AuthContext.Provider value={authStore}>
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
       {props.children}
     </AuthContext.Provider>
   );
