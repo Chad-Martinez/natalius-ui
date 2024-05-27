9ab6b207119d70427145045f837046e530eb6a25
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-27 13:53:44 -0600

    added link to income form

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,14 +2,23 @@ import { FC } from 'react';
 import styles from './PageWrapper.module.css';
 import BottomNav from '../components/dashboard/BottomNav';
 import Button from '../components/ui/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard: FC = (): JSX.Element => {
+  const navigate = useNavigate();
+  const handleAddIncome = () => {
+    navigate('/income/form');
+  };
+
+  const handleAddExpense = () => {
+    // navigate('/expense/form');
+  };
   return (
     <div className={styles.container}>
       <div className={styles.mainContent}></div>
       <BottomNav>
-        <Button text='Add Income' />
-        <Button text='Add Expense' />
+        <Button text='Add Income' onClick={handleAddIncome} />
+        <Button text='Add Expense' onClick={handleAddExpense} />
       </BottomNav>
     </div>
   );
