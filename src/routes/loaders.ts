9a6e5ded40f5e2edf8d110f81e5cd6968673ef6c
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-27 13:52:19 -0600

    moved loaders into its own file and added gigNames loader function for the income form

---
+++ src/routes/loaders.ts
@@ -0,0 +1,21 @@
+import { getGigNames, getGigs } from '../services/gigsServices';
+
+export const gigNamesLoader = async () => {
+  try {
+    const { data } = await getGigNames();
+    return data;
+  } catch (error) {
+    console.error('Loader Error: Gig Names ', error);
+    return error;
+  }
+};
+
+export const gigsLoader = async () => {
+  try {
+    const { data } = await getGigs();
+    return data;
+  } catch (error) {
+    console.error('Loader Error: Get Gigs ', error);
+    return error;
+  }
+};
