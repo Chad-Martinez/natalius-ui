41c1e89147a144400d5aad9e7c68857d2e68b1f6
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-27 17:40:34 -0600

    commented out code without functionality behind the feature

--- src/pages/Gigs/Gigs.tsx
+++ src/pages/Gigs/Gigs.tsx
@@ -1,32 +1,26 @@
 import { FC } from 'react';
-import { getGigs } from '../../services/gigsServices';
+// import { useNavigate } from 'react-router-dom';
 import GigsList from './components/GigsList';
 import BottomNav from '../../components/dashboard/BottomNav';
 import Button from '../../components/ui/Button';
 import styles from '../PageWrapper.module.css';
 
 const Gigs: FC = (): JSX.Element => {
+  // const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    // navigate('form')
+  };
   return (
     <div className={styles.container}>
       <div className={styles.mainContent}>
         <GigsList />
       </div>
       <BottomNav>
-        <Button text='Add Gig' />
+        <Button text='Add Gig' onClick={handleNavigate} />
       </BottomNav>
     </div>
   );
 };
 
 export default Gigs;
-
-export const loader = async () => {
-  try {
-    const { data } = await getGigs();
-    console.log('data ', data);
-    return data;
-  } catch (error) {
-    console.error('Loader Error: GetGigs ', error);
-    return error;
-  }
-};
