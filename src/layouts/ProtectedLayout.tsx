3f0b16905fa5bb574343ad1c4b9741813887645a
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-31 12:12:59 -0600

    refactored protected layout to only navigate back to login after refresh token check is complete.

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -7,13 +7,9 @@ import {
   useEffect,
   useState,
 } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from '../store/AuthContext';
 import TopNav from '../components/dashboard/TopNav';
-import axios from 'axios';
-import { refresh } from '../services/tokenServices.ts';
-
-axios.defaults.withCredentials = true;
 
 export const MenuContext = createContext<{
   isOpen: boolean;
@@ -26,54 +22,9 @@ export const MenuContext = createContext<{
 const ProtectedLayout: FC = (): JSX.Element => {
   const [title, setTitle] = useState<string>('');
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { isAuth, setIsAuth } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { isAuth, isLoading } = useContext(AuthContext);
+
   const { pathname } = useLocation();
-
-  useEffect(() => {
-    const requestInterceptor = axios.interceptors.request.use(
-      (config) => {
-        const accessToken = sessionStorage.getItem('at');
-        if (accessToken) {
-          if (!config.headers['Authorization']) {
-            config.headers['Authorization'] = `Bearer ${accessToken}`;
-          }
-        }
-        return config;
-      },
-      (error) => Promise.reject(error)
-    );
-
-    const responseInterceptor = axios.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const prevRequest = error?.config;
-        if (error.response.status === 403 && !prevRequest.sent) {
-          prevRequest.sent = true;
-          const { data } = await refresh();
-          const { accessToken } = data;
-
-          sessionStorage.setItem('at', accessToken);
-          prevRequest.headers['Authorization'] = `Bearer ${accessToken}`;
-          return axios(prevRequest);
-        }
-        if (error.response.status === 418) {
-          console.error('Invalid Token ', error);
-          setIsAuth(false);
-        }
-        return Promise.reject(error);
-      }
-    );
-
-    return () => {
-      axios.interceptors.request.eject(requestInterceptor);
-      axios.interceptors.response.eject(responseInterceptor);
-    };
-  }, [setIsAuth]);
-
-  useEffect(() => {
-    if (!isAuth) return navigate('/login');
-  }, [navigate, isAuth]);
 
   useEffect(() => {
     const path = pathname.split('/');
@@ -84,13 +35,17 @@ const ProtectedLayout: FC = (): JSX.Element => {
     if (isOpen) setIsOpen(false);
   };
 
-  return (
+  return isAuth ? (
     <div onClick={handleToggle}>
       <MenuContext.Provider value={{ isOpen, setIsOpen }}>
         <TopNav title={title} />
         <Outlet />
       </MenuContext.Provider>
     </div>
+  ) : isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <Navigate to='/login' replace />
   );
 };
 
