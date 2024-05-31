af60f36734653e8471947ad5c38a05c9459a5e49
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-31 12:07:55 -0600

    implemented check to verify refresh token valid and keep user logged in and on the same route

--- src/store/AuthContext.tsx
+++ src/store/AuthContext.tsx
@@ -1,26 +1,57 @@
+import axios from 'axios';
 import {
   Dispatch,
   FC,
   PropsWithChildren,
   SetStateAction,
   createContext,
+  useEffect,
+  useMemo,
   useState,
 } from 'react';
+import useRefreshToken from '../hooks/useRefreshToken';
 
 export const AuthContext = createContext<{
   isAuth: boolean;
   setIsAuth: Dispatch<SetStateAction<boolean>> | ((value: boolean) => void);
+  isLoading: boolean;
 }>({
   isAuth: false,
   setIsAuth: () => {},
+  isLoading: true,
 });
 
 export const AuthProvider: FC<PropsWithChildren> = (props) => {
   const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { verifyRefreshToken } = useRefreshToken();
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        const accessToken = await verifyRefreshToken();
+        axios.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${accessToken}`;
+        setIsAuth(true);
+      } catch (error) {
+        console.error('Token Error ', error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [verifyRefreshToken]);
+
+  const store = useMemo(() => {
+    return {
+      isAuth,
+      setIsAuth,
+      isLoading,
+    };
+  }, [isAuth, setIsAuth, isLoading]);
 
   return (
-    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
-      {props.children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={store}>{props.children}</AuthContext.Provider>
   );
 };
