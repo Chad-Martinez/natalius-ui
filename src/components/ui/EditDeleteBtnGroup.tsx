cb3e1e31f02e4b9ac26ad6b798acf779ae186dd7
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-02 01:53:20 -0600

    refactored button group to handle modal

--- src/components/ui/EditDeleteBtnGroup.tsx
+++ src/components/ui/EditDeleteBtnGroup.tsx
@@ -1,13 +1,23 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBoxArchive,
+  faPencil,
+  // faTrashCan,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from './EditDeleteBtnGroup.module.css';
+import { ButtonContext } from '../../pages/Gigs/components/GigItem';
 
 const EditDeleteBtnGroup: FC = (): JSX.Element => {
+  const { handleModal } = useContext(ButtonContext);
   return (
     <div className={styles.iconGroup}>
       <FontAwesomeIcon className={styles.faIcon} icon={faPencil} />
-      <FontAwesomeIcon className={styles.faIcon} icon={faTrashCan} />
+      <FontAwesomeIcon
+        className={styles.faIcon}
+        icon={faBoxArchive}
+        onClick={handleModal}
+      />
     </div>
   );
 };
