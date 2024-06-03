8c1367372cd336c42bf7f0310ef0ecd44260b95c
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-02 21:04:13 -0600

    removed use of context for editdeletebtngroup

--- src/components/ui/EditDeleteBtnGroup.tsx
+++ src/components/ui/EditDeleteBtnGroup.tsx
@@ -1,26 +1,28 @@
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBoxArchive,
   faPencil,
-  // faTrashCan,
+  faTrashCan,
 } from '@fortawesome/free-solid-svg-icons';
 import styles from './EditDeleteBtnGroup.module.css';
-import { ButtonContext } from '../../pages/Gigs/components/GigItem';
 
-const EditDeleteBtnGroup: FC = (): JSX.Element => {
-  const { handleModal, handleEdit } = useContext(ButtonContext);
+const EditDeleteBtnGroup: FC<{
+  archiveBtn?: boolean;
+  onEdit: () => void;
+  onDelete: () => void;
+}> = ({ archiveBtn = false, onEdit, onDelete }): JSX.Element => {
   return (
     <div className={styles.iconGroup}>
       <FontAwesomeIcon
         className={styles.faIcon}
         icon={faPencil}
-        onClick={handleEdit}
+        onClick={onEdit}
       />
       <FontAwesomeIcon
         className={styles.faIcon}
-        icon={faBoxArchive}
-        onClick={handleModal}
+        icon={archiveBtn ? faBoxArchive : faTrashCan}
+        onClick={onDelete}
       />
     </div>
   );
--- src/pages/Gigs/components/GigItem.tsx
+++ src/pages/Gigs/components/GigItem.tsx
@@ -1,4 +1,4 @@
-import { FC, createContext, useRef } from 'react';
+import { FC, useRef } from 'react';
 import styles from './GigItem.module.css';
 import {
   faClock,
@@ -18,14 +18,6 @@ import Modal from '../../../components/ui/Modal/Modal';
 import { IGig } from '../../../interfaces/IGig.interface';
 import { IHTMLDialogElement } from '../../../interfaces/IHTMLDialog.interface';
 
-export const ButtonContext = createContext<{
-  handleModal: () => void;
-  handleEdit: () => void;
-}>({
-  handleModal: () => {},
-  handleEdit: () => {},
-});
-
 const GigItem: FC<{ gig: IGig; archiveGig: (payload: IGig) => void }> = ({
   gig,
   archiveGig,
@@ -34,11 +26,11 @@ const GigItem: FC<{ gig: IGig; archiveGig: (payload: IGig) => void }> = ({
   const navigate = useNavigate();
   const dialogRef = useRef<IHTMLDialogElement | null>(null);
 
-  const handleModal = (): void => {
+  const openModal = (): void => {
     dialogRef.current?.openModal();
   };
 
-  const handleArchive = () => {
+  const handleArchive = (): void => {
     const payload: IGig = {
       _id: gig._id,
       isArchived: true,
@@ -53,38 +45,35 @@ const GigItem: FC<{ gig: IGig; archiveGig: (payload: IGig) => void }> = ({
 
   return (
     <>
-      <ButtonContext.Provider value={{ handleModal, handleEdit }}>
-        <Modal ref={dialogRef} title='Archive Gig?' onConfirm={handleArchive} />
-
-        <Card addedStyles={{ maxWidth: '607.5px' }}>
-          <CardHeader text={name} />
-          <CardContent>
-            {contact?.name && (
-              <CardContentItem text={contact.name} icon={faUser} />
-            )}
-            {contact?.phone && (
-              <CardContentItem text={contact.phone} icon={faPhone} />
-            )}
-            {fullAddress && (
-              <CardContentItem text={fullAddress} icon={faLocationDot} />
-            )}
-            {shifts && shifts.length > 0 && (
-              <CardContentAccordian
-                text='Scheduled Shifts'
-                icon={faClock}
-                enabled={shifts.length > 0}
-              >
-                {shifts.length > 0 ? <ShiftsList shifts={shifts} /> : ''}
-              </CardContentAccordian>
-            )}
-          </CardContent>
-          <div className={styles.addGig}>
-            <Link className={styles.shiftLink} to={`shift-form/${gig._id}`}>
-              Add Shift
-            </Link>
-          </div>
-        </Card>
-      </ButtonContext.Provider>
+      <Modal ref={dialogRef} title='Archive Gig?' onConfirm={handleArchive} />
+      <Card addedStyles={{ maxWidth: '607.5px' }}>
+        <CardHeader text={name} onEdit={handleEdit} onDelete={openModal} />
+        <CardContent>
+          {contact?.name && (
+            <CardContentItem text={contact.name} icon={faUser} />
+          )}
+          {contact?.phone && (
+            <CardContentItem text={contact.phone} icon={faPhone} />
+          )}
+          {fullAddress && (
+            <CardContentItem text={fullAddress} icon={faLocationDot} />
+          )}
+          {shifts && shifts.length > 0 && (
+            <CardContentAccordian
+              text='Scheduled Shifts'
+              icon={faClock}
+              enabled={shifts.length > 0}
+            >
+              {shifts.length > 0 ? <ShiftsList shiftData={shifts} /> : ''}
+            </CardContentAccordian>
+          )}
+        </CardContent>
+        <div className={styles.addGig}>
+          <Link className={styles.shiftLink} to={`shift-form/${gig._id}`}>
+            Add Shift
+          </Link>
+        </div>
+      </Card>
     </>
   );
 };
