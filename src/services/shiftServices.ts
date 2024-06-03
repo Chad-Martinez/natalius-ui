8a0d023c7bddcfa6a3569567b87fd94de5f6f19a
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-02 21:02:27 -0600

    created update and delete shift services

--- src/services/shiftServices.ts
+++ src/services/shiftServices.ts
@@ -1,12 +1,39 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { Shift } from '../types/Shift';
+import { IShift, IShiftBase } from '../interfaces/IShift.interface';
 
 const endpoint = process.env.API_ENDPOINT;
 
-export const addShift = async (payload: Shift): Promise<AxiosResponse> => {
+export const addShift = async (payload: IShiftBase): Promise<AxiosResponse> => {
   const config: AxiosRequestConfig = {
     url: `${endpoint}/shifts`,
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: payload,
+  };
+  return await axios(config);
+};
+
+export const updateShift = async (payload: IShift) => {
+  const config: AxiosRequestConfig = {
+    url: `${endpoint}/shifts`,
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: payload,
+  };
+  return await axios(config);
+};
+
+export const deleteShift = async (payload: {
+  shiftId: IShift['_id'];
+  gigId: IShift['gigId'];
+}): Promise<AxiosResponse> => {
+  const config: AxiosRequestConfig = {
+    url: `${endpoint}/shifts`,
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
