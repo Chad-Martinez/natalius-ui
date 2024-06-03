910c7425e54a57ac256a9a64fa48b9f1cd6091b0
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-02 21:03:42 -0600

    implemented shift delete and update

--- src/pages/Gigs/components/ShiftListItem.tsx
+++ src/pages/Gigs/components/ShiftListItem.tsx
@@ -1,19 +1,57 @@
-import { FC } from 'react';
-import { Shift } from '../../../types/Shift';
+import { FC, useRef } from 'react';
 import dayjs from 'dayjs';
 import styles from './ShiftsList.module.css';
 import EditDeleteBtnGroup from '../../../components/ui/EditDeleteBtnGroup';
+import Modal from '../../../components/ui/Modal/Modal';
+import { IHTMLDialogElement } from '../../../interfaces/IHTMLDialog.interface';
+import { IShift } from '../../../interfaces/IShift.interface';
+import { useNavigate } from 'react-router-dom';
 
-const ShiftListItem: FC<{ shift: Shift }> = ({ shift }): JSX.Element => {
+const ShiftListItem: FC<{
+  shift: IShift;
+  handleDelete: (payload: {
+    shiftId: IShift['_id'];
+    gigId: IShift['gigId'];
+  }) => Promise<void>;
+}> = ({ shift, handleDelete }): JSX.Element => {
+  const dialogRef = useRef<IHTMLDialogElement | null>(null);
+  const navigate = useNavigate();
+
+  const handleEdit = () => {
+    navigate(`shift-form/${shift.gigId}`, { state: { shift } });
+  };
+
+  const openModal = (): void => {
+    dialogRef.current?.openModal();
+  };
+
+  const deleteShift = (): void => {
+    const payload: { shiftId: IShift['_id']; gigId: IShift['gigId'] } = {
+      shiftId: shift._id,
+      gigId: shift.gigId,
+    };
+    handleDelete(payload);
+    dialogRef.current?.closeModal();
+  };
   return (
-    <div className={styles.shiftListItem}>
-      <div>
-        {dayjs(shift.start).format('ddd: MMM D')} @{' '}
-        {dayjs(shift.start).format('h:mma')} -{' '}
-        {dayjs(shift.end).format('h:mma')}
+    <>
+      <Modal
+        ref={dialogRef}
+        title='Delete Shift?'
+        subtitle='This action cannot be undone'
+        onConfirm={deleteShift}
+      />
+      <div className={styles.shiftListItem}>
+        <div>
+          {dayjs(shift.start).format('ddd: MMM D')} @{' '}
+          {dayjs(shift.start).format('h:mma')} -{' '}
+          {dayjs(shift.end).format('h:mma')}
+        </div>
+        <div>
+          {<EditDeleteBtnGroup onEdit={handleEdit} onDelete={openModal} />}
+        </div>
       </div>
-      <div>{<EditDeleteBtnGroup />}</div>
-    </div>
+    </>
   );
 };
 
--- src/pages/Gigs/components/ShiftsList.tsx
+++ src/pages/Gigs/components/ShiftsList.tsx
@@ -1,13 +1,44 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import ShiftListItem from './ShiftListItem';
-import { Shift } from '../../../types/Shift';
 import styles from './ShiftsList.module.css';
+import { deleteShift } from '../../../services/shiftServices';
+import { notify } from '../../../utils/toastify';
+import { AxiosError } from 'axios';
+import { IShift } from '../../../interfaces/IShift.interface';
 
-const ShiftsList: FC<{ shifts: Shift[] }> = ({ shifts }): JSX.Element => {
+const ShiftsList: FC<{ shiftData: IShift[] }> = ({
+  shiftData,
+}): JSX.Element => {
+  const [shifts, setShifts] = useState<IShift[]>(shiftData);
+
+  const handleDelete = async (payload: {
+    shiftId: IShift['_id'];
+    gigId: IShift['gigId'];
+  }) => {
+    try {
+      const response = await deleteShift(payload);
+      console.log('resposne ', response);
+      const filteredShifts = shifts.filter(
+        (shift) => shift._id !== payload.shiftId
+      );
+      setShifts(filteredShifts);
+      notify('Shift deleted', 'success', 'delete-shift-success');
+    } catch (error) {
+      console.error('Delete Shift Error: ', error);
+      if (error instanceof AxiosError)
+        notify(error.response?.data.message, 'error', 'delete-shift-error');
+    }
+  };
   return (
     <div className={styles.shiftList}>
       {shifts.length > 0
-        ? shifts.map((shift) => <ShiftListItem key={shift._id} shift={shift} />)
+        ? shifts.map((shift) => (
+            <ShiftListItem
+              key={shift._id}
+              shift={shift}
+              handleDelete={handleDelete}
+            />
+          ))
         : ''}
     </div>
   );
