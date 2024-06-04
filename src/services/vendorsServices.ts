410c223d9083f731b3d84527490d172605e27293
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-03 23:13:30 -0600

    added vendor service

---
+++ src/services/vendorsServices.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { IVendorBase } from '../interfaces/IVendor.interface';
+
+const endpoint = process.env.API_ENDPOINT;
+
+export const addVendor = async (
+  payload: IVendorBase
+): Promise<AxiosResponse> => {
+  const config: AxiosRequestConfig = {
+    url: `${endpoint}/vendors`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: payload,
+  };
+  return await axios(config);
+};
