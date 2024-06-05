447d2ed72f0f4639039fa28aede0348a3c6ce1b7
chadcito <145295470+chadcito@users.noreply.github.com>
2024-06-04 18:22:17 -0600

    implemented widget in expenses page

--- src/pages/Expenses/Expenses.tsx
+++ src/pages/Expenses/Expenses.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
-import styles from '../PageWrapper.module.css';
+import pageStyles from '../PageWrapper.module.css';
 import BottomNav from '../../components/dashboard/BottomNav';
 import Button from '../../components/ui/Button';
 import { useNavigate } from 'react-router-dom';
+import MonthlyBudgetWidget from './components/MonthlyBudgetWidget';
+import styles from './Expenses.module.css';
 
 const Expenses: FC = (): JSX.Element => {
   const navigate = useNavigate();
@@ -11,8 +13,12 @@ const Expenses: FC = (): JSX.Element => {
     navigate('expense-form');
   };
   return (
-    <div className={styles.container}>
-      <div className={styles.mainContent}>Expenses</div>
+    <div className={pageStyles.container}>
+      <div className={pageStyles.mainContent}>
+        <div className={styles.widgetContainer}>
+          <MonthlyBudgetWidget />
+        </div>
+      </div>
       <BottomNav>
         <Button text='Add Expense' onClick={handleAddExpense} />
       </BottomNav>
