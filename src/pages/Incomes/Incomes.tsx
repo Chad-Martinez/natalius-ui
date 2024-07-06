e52ec5bf4b80e8c28ca1ea9321f0c8feecee6a93
chadcito <145295470+chadcito@users.noreply.github.com>
2024-07-05 22:42:49 -0600

    updated graph type

--- src/pages/Incomes/Incomes.tsx
+++ src/pages/Incomes/Incomes.tsx
@@ -10,12 +10,14 @@ import { ISprint } from '../../interfaces/ISprint.interface';
 import { IncomeAverages } from '../../types/IncomeAverages';
 import SprintGoalWidget from '../../components/Widgets/SprintGoalWidget';
 import BarGraphWidget from '../../components/Widgets/BarGraphWidget';
+import { BarGraphData } from '../../types/GraphData';
 
 const Incomes: FC = (): JSX.Element => {
   const navigate = useNavigate();
   const incomeLoaderData = useLoaderData() as {
     sprint: ISprint;
     averages: IncomeAverages;
+    graphData: BarGraphData;
   };
 
   const handleAddIncome = () => {
@@ -28,7 +30,7 @@ const Incomes: FC = (): JSX.Element => {
         <div className={widgetStyles.widgetContainer}>
           <SprintGoalWidget sprint={incomeLoaderData?.sprint} />
           <AveragesWidget averages={incomeLoaderData?.averages} />
-          <BarGraphWidget />
+          <BarGraphWidget graphLoaderData={incomeLoaderData?.graphData} />
         </div>
       </div>
       <BottomNav>
