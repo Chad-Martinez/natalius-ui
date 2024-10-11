0ef725c8b02a20a91427c4239131e838cb3aef2f
chadcito <145295470+chadcito@users.noreply.github.com>
2024-10-10 18:15:46 -0600

    removed add income button from income page

--- src/pages/Incomes/Incomes.tsx
+++ src/pages/Incomes/Incomes.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import pageStyles from '../PageWrapper.module.css';
 import widgetStyles from '../../components/Widgets/Widget.module.css';
-import BottomNav from '../../components/dashboard/BottomNav';
-import Button from '../../components/ui/Button/Button';
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import PageHeader from '../../components/ui/PageHeader/PageHeader';
 import AveragesWidget from '../../components/Widgets/AveragesWidget/AveragesWidget';
 import { ISprint } from '../../interfaces/ISprint.interface';
@@ -13,29 +11,22 @@ import BarGraphWidget from '../../components/Widgets/BarGraphWidget';
 import { BarGraphData } from '../../types/GraphData';
 
 const Incomes: FC = (): JSX.Element => {
-  const navigate = useNavigate();
   const incomeLoaderData = useLoaderData() as {
     sprint: ISprint;
     averages: IncomeAverages;
     graphData: BarGraphData;
   };
 
-  const handleAddIncome = () => {
-    navigate('income-form');
-  };
   return (
     <>
       <div className={pageStyles.mainContent}>
         <PageHeader linkRight='view-income' linkRightText='View Income' />
         <div className={widgetStyles.widgetContainer}>
-          <SprintGoalWidget sprint={incomeLoaderData?.sprint} />
+          <SprintGoalWidget sprintData={incomeLoaderData?.sprint} />
           <AveragesWidget averages={incomeLoaderData?.averages} />
           <BarGraphWidget graphLoaderData={incomeLoaderData?.graphData} />
         </div>
       </div>
-      <BottomNav>
-        <Button text='Add Income' onClick={handleAddIncome} />
-      </BottomNav>
     </>
   );
 };
