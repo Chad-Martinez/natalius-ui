725b31153935674a8c5d6a576808475bab285ca2
Chad Martinez <cmartinez@blackburnlabs.com>
2024-11-10 23:59:32 -0500

    closed menu popup after click on a nav link from the top nav menu

--- src/components/ui/Menu/MenuContainer.tsx
+++ src/components/ui/Menu/MenuContainer.tsx
@@ -3,9 +3,11 @@ import styles from './MenuContainer.module.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { logout } from '../../../services/authServices';
 import { AuthContext } from '../../../store/AuthContext';
+import { MenuContext } from '../../../layouts/ProtectedLayout';
 
 const MenuContainer: FC<{ open: boolean }> = ({ open }): JSX.Element => {
   const { setIsAuth } = useContext(AuthContext);
+  const { setShowPopup } = useContext(MenuContext);
   const navigate = useNavigate();
 
   const handleLogout = async (): Promise<void> => {
@@ -25,30 +27,35 @@ const MenuContainer: FC<{ open: boolean }> = ({ open }): JSX.Element => {
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to='/dashboard'
+        onClick={() => setShowPopup(false)}
       >
         <li>Dashboard</li>
       </NavLink>
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to='/profile'
+        onClick={() => setShowPopup(false)}
       >
         <li>Profile</li>
       </NavLink>
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to='/income'
+        onClick={() => setShowPopup(false)}
       >
         <li>Income</li>
       </NavLink>
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to='/expenses'
+        onClick={() => setShowPopup(false)}
       >
         <li>Expenses</li>
       </NavLink>
       <NavLink
         className={({ isActive }) => (isActive ? styles.active : undefined)}
         to='/clubs'
+        onClick={() => setShowPopup(false)}
       >
         <li>Clubs</li>
       </NavLink>
