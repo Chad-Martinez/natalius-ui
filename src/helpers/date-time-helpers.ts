c6fd0a9a1037ac0e472936cda5cc3073829d2fa8
Chad Martinez <cmartinez@blackburnlabs.com>
2024-11-12 16:07:39 -0500

    added time zone select array and getUserTimeZone helper function

--- src/helpers/date-time-helpers.ts
+++ src/helpers/date-time-helpers.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 export const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 export const WEEKS_OF_MONTH = [
@@ -22,3 +24,46 @@ export const MONTHS_OF_YEAR = [
   'Nov',
   'Dec',
 ];
+
+export const TIMEZONE_SELECT = [
+  {
+    _id: 'America/Los_Angeles',
+    name: 'Pacific Standard Time (PST)',
+  },
+  {
+    _id: 'America/Denver',
+    name: 'Mountain Standard Time (MST)',
+  },
+  {
+    _id: 'America/Chicago',
+    name: 'Central Standard Time (CST)',
+  },
+  {
+    _id: 'America/New_York',
+    name: 'Eastern Standard Time (EST)',
+  },
+];
+
+export const getUserTimezone = () => {
+  try {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  } catch (e) {
+    try {
+      return dayjs.tz.guess();
+    } catch (e) {
+      const offset = -new Date().getTimezoneOffset() / 60;
+      const gmtOffset = `GMT ${offset}`;
+
+      if (offset === -4 || offset === -5) {
+        return 'America/New_York';
+      } else if (offset === -5 || offset === -6) {
+        return 'America/Chicago';
+      } else if (offset === -6 || offset === -7) {
+        return 'America/Denver';
+      } else if (offset === -7 || offset === -8) {
+        return 'America/Los_Angeles';
+      }
+      return gmtOffset;
+    }
+  }
+};
