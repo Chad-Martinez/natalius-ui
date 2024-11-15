7667e33820cc6d2970b94cc32d909c18b5a09185
Chad Martinez <cmartinez@blackburnlabs.com>
2024-11-14 21:37:24 -0500

    removed unused income services

--- src/services/incomeServices.ts
+++ src/services/incomeServices.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { IIncome, IIncomeBase } from '../interfaces/IIncome.interface';
 
 const endpoint = process.env.API_ENDPOINT;
 
@@ -21,45 +20,6 @@ export const getIncomeDashboardData = async () => {
   const config: AxiosRequestConfig = {
     url: `${endpoint}/income/dashboard`,
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  return await axios(config);
-};
-
-export const addIncome = async (
-  payload: IIncomeBase
-): Promise<AxiosResponse> => {
-  const config: AxiosRequestConfig = {
-    url: `${endpoint}/income`,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: payload,
-  };
-  return await axios(config);
-};
-
-export const updateIncome = async (
-  payload: IIncome
-): Promise<AxiosResponse> => {
-  const config: AxiosRequestConfig = {
-    url: `${endpoint}/income`,
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: payload,
-  };
-  return await axios(config);
-};
-
-export const deleteIncome = async (_id: string): Promise<AxiosResponse> => {
-  const config: AxiosRequestConfig = {
-    url: `${endpoint}/income/${_id}`,
-    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
