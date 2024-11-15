ef8e9ad8f25a06d44444f51252b8918f867b4643
Chad Martinez <cmartinez@blackburnlabs.com>
2024-11-14 21:25:59 -0500

    added IDefaults interface for IClub

--- src/interfaces/IClub.interface.ts
+++ src/interfaces/IClub.interface.ts
@@ -1,17 +1,31 @@
 import { IShift } from './IShift.interface';
 
+interface IAddress {
+  street?: string | null;
+  city?: string | null;
+  state?: string | null;
+  zip?: number | null;
+}
+
+interface IContact {
+  name?: string | null;
+  phone?: string | null;
+}
+
+interface IDefaults {
+  useDefaults: boolean;
+  floorFee: number;
+  pricePerDance: number;
+  tips: number;
+  other: number;
+  distance: number;
+  timezone: string;
+}
+
 export interface IClubBase {
-  address?: {
-    street?: string;
-    city?: string;
-    state?: string;
-    zip?: number;
-  };
-  contact?: {
-    name?: string;
-    phone?: string;
-  };
-  distance?: number;
+  address: IAddress;
+  contact: IContact;
+  defaults: IDefaults;
   name: string;
   isArchived: boolean;
 }
