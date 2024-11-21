f663fcfe13432b6495c116c7cc21a79947e066e1
chadcito <145295470+chadcito@users.noreply.github.com>
2024-11-21 03:08:16 -0700

    created nav-menu helper

---
+++ src/helpers/nav-menu-helpers.ts
@@ -0,0 +1,8 @@
+export const LINKS: { name: string; path: string }[] = [
+  { name: 'Dashboard', path: '/dashboard' },
+  { name: 'Income', path: '/income' },
+  { name: 'Expenses', path: '/expenses' },
+  { name: 'Clubs', path: '/clubs' },
+  { name: 'Profile', path: '/profile' },
+  { name: 'Logout', path: '/logout' },
+];
