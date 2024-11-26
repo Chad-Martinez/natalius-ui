c9faf6cd196552407bd0c0d95a5c69fcce1cd80c
chadcito <145295470+chadcito@users.noreply.github.com>
2024-11-25 19:51:11 -0700

    changed period prop to title for better description, added injectable styles to title and amount divs, added intersection observer api to ensure animation does not play into visible

--- src/components/ui/Counter/Counter.tsx
+++ src/components/ui/Counter/Counter.tsx
@@ -1,17 +1,54 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import styles from './Counter.module.css';
 import { moneyFormatter } from '../../../helpers/format-helpers';
 
 const Counter: FC<{
-  period: string;
+  title: string;
   amount: number;
   interval: number;
   skip: number;
-}> = ({ period, amount, interval, skip }): JSX.Element => {
+  amountStyles?: { [key: string]: string };
+  titleStyles?: { [key: string]: string };
+}> = ({
+  title,
+  amount,
+  interval,
+  skip,
+  amountStyles,
+  titleStyles,
+}): JSX.Element => {
   const [count, setCount] = useState<number>(0);
   const [isFinished, setIsFinished] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const counterDivRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+      }
+    );
+
+    const counterElement = counterDivRef.current;
+
+    if (counterElement) {
+      observer.observe(counterElement);
+    }
+
+    return () => {
+      if (counterElement) {
+        observer.unobserve(counterElement);
+      }
+    };
+  }, [counterDivRef]);
+
+  useEffect(() => {
+    if (!isVisible) return;
     const int = setInterval(() => {
       setCount((prevCount) => (prevCount += skip));
     }, interval);
@@ -22,15 +59,26 @@ const Counter: FC<{
     }
 
     return () => clearInterval(int);
-  }, [amount, count, interval, skip]);
+  }, [amount, count, interval, skip, isVisible]);
 
   return (
-    <>
-      <div className={`${styles.amount} ${isFinished ? styles.slam : ''}`}>
-        {`$${moneyFormatter(count)}`}
-      </div>
-      <div className={styles.counter}>{period}</div>
-    </>
+    <div ref={counterDivRef}>
+      {isVisible ? (
+        <>
+          <div
+            className={`${styles.amount} ${isFinished ? styles.slam : ''}`}
+            style={{ ...amountStyles }}
+          >
+            {`$${moneyFormatter(count)}`}
+          </div>
+          <div className={styles.title} style={{ ...titleStyles }}>
+            {title}
+          </div>
+        </>
+      ) : (
+        ''
+      )}
+    </div>
   );
 };
 
