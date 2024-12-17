e219d12e73f0cfb1f758d5e15ada54f12428f036
chadcito <145295470+chadcito@users.noreply.github.com>
2024-12-16 18:36:02 -0700

    removed first and last names for stage name

--- src/services/profileServices.ts
+++ src/services/profileServices.ts
@@ -1,13 +1,13 @@
 import { AxiosResponse } from 'axios';
 import axiosInstance from './axiosConfig';
+import { Register } from '../types/auth-services-types';
 
 export const getUserInfo = async (): Promise<AxiosResponse> =>
   await axiosInstance.get('/profiles');
 
 export const updateUserInfo = async (payload: {
-  firstName: string;
-  lastName: string;
-  email: string;
+  stageName: Register['stageName'];
+  email: Register['email'];
 }): Promise<AxiosResponse> => await axiosInstance.put('/profiles', payload);
 
 export const updatePassword = async (payload: {
